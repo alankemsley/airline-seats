e2eb335a2c1133254c0545a9c756393ea914ecce
Alan Kemsley <alanjkemsley@gmail.com>
2018-01-01 17:17:36 -0500

    Create initial project files.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <main className="App-main">
+            <h1 className="App-title">SELECT SEAT</h1>
+            {/* <Aircraft /> */}
+          </main>
+        </div>
+        <footer className="App-footer">
+          <p>MADE BY ALAN KEMSLEY</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
